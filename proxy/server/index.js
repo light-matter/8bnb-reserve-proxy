2549a6898d4159be000a20551fb890cc06458b41
Gabriel Song <gabsong4@gmail.com>
2020-02-04 13:26:13 -0800

    Restructure app to contain proxy server in its own directory

---
+++ proxy/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(morgan('dev')); // for logging http requests to the terminal
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use('/', express.static(path.join(__dirname, '../public'))); // for serving static files
+
+app.listen(port, () => console.log(`App in mode: '${process.env.NODE_ENV}' listening on port ${port}!`));
